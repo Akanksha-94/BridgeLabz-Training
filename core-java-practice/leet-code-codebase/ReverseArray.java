9438610675fc8e3d929baf268d0cb6eb387dff0f
Akanksha-94 <akankshabhardwaj447@gmail.com>
2025-12-17 15:56:47 +0530

    [Akanksha Bhardwaj] leetcode basic problems

---
+++ core-java-practice/leet-code-codebase/ReverseArray.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void reverse(int[] arr) {
+
+        int i = 0;
+        int j = arr.length - 1;
+
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverse(arr);
+
+        System.out.println("Reversed array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
