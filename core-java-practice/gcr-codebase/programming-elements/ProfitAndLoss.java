ed00e988f67aca7dfaee7006dd64507af8341976
Akanksha-94 <akankshabhardwaj447@gmail.com>
2025-12-18 22:17:04 +0530

    [Akanksha Bhardwaj] class problem level 1 and level 2

---
+++ core-java-practice/gcr-codebase/programming-elements/ProfitAndLoss.java
@@ -0,0 +1,14 @@
+public class ProfitAndLoss {
+  
+
+public static void main(String[] args) {
+  double costPrice = 129;
+  double sellingPrice = 191;
+  
+  double profit = sellingPrice - costPrice;
+  double profitPercentage = (profit / costPrice) * 100;
+  
+  System.out.print("TheCose Price is INR "+ costPrice+ "and Selling Price is INR " + sellingPrice +" the Profit Percentage is: " + profitPercentage + "%" +"and the Profit Percentage is "+ profitPercentage+"%");
+
+}
+}
