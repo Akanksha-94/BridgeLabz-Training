abf9e36fbde5d6abbb9e4653ae83204f214174b8
Akanksha-94 <akankshabhardwaj447@gmail.com>
2025-12-30 16:48:00 +0530

    [Akanksha Bhardwaj] Refactor . solved EmployeeWage problem

--- core-java-practice/scenario-based/EmployeeWageComputationProblem.java
+++ core-java-practice/scenario-based/EmployeeWageComputationProblem.java
@@ -1,75 +1,90 @@
-import java.util.Random;
-public class EmployeeWageComputationProblem {
+import java.util.Scanner;
+
+public class EmployeeWagerComputation {
+
+  private static final int WAGE_PER_HOUR = 20;
+  private static final int FULL_TIME_HOUR = 8;
+  private static final int PART_TIME_HOUR = 8;
+  private static final int WORKING_DAYS = 20;
+  private static final int MAX_HOURS = 100;
+
+  public static void main(String[] args) {
+    System.out.println("Welcom to Emplyee Wage Computation");
+
+    int dailyWage = calculateDailyWage();
+    System.out.println("Daily Wage: " + dailyWage);
+
+    System.out.println("Employee type: ");
+    System.out.println("1. part time ");
+    System.out.println("2. full time ");
+
+    Scanner sc = new Scanner(System.in);
+    int employeeType = sc.nextInt();
+
+    int hours = getHours(employeeType);
+    int wage = calculateWage(hours);
+    System.out.println("Wage for selected employee type: " + wage);
+
+    int monthlyWages = calculateMonthlyWage();
+    System.out.println("Monthly wages:" + monthlyWages);
+
+    int wageWithLimits=calculateWageUntilLimit();
+    System.out.println("wages until limit:" + wageWithLimits);
 
 
+  }
 
+  private static boolean isEmployeePresent() {
 
-    static int WAGE_PER_HOUR = 20;
-    static int FULL_DAY_HOURS = 8;
-    static int PART_TIME_HOURS = 8; 
+    int attendance = (int) (Math.random() * 2);
+    return attendance ==  1;
+  }
 
-    public static void main(String[] args) {
+  static int calculateDailyWage() {
 
-        displayWelcomeMessage();
+    if (isEmployeePresent()) {
+      return WAGE_PER_HOUR *  FULL_TIME_HOUR;
+    }
+    return 0;
+  }
 
-        int employeeType = getEmployeeType();
+  static int calculateWage(int hours) {
+    return WAGE_PER_HOUR * hours;
+  }
 
-        int dailyWage = calculateDailyWage(employeeType);
+  static int getHours(int empType) {
+    switch (empType) {
+      case 1:
+        return FULL_TIME_HOUR;
+      case 2:
+        return PART_TIME_HOUR;
+      default:
+        return 0;
+    }
+  }
 
-        displayWage(employeeType, dailyWage);
+  static int calculateMonthlyWage() {
+    int totalWage = 0;
+    for (int day = 1; day <= WORKING_DAYS; day++) {
+      totalWage += calculateDailyWage();
+    }
+    return totalWage;
+  }
+
+  static int calculateWageUntilLimit() {
+    int totalWage = 0;
+    int totalHours = 0;
+    int days = 0;
+
+    while (totalHours < MAX_HOURS && days < WORKING_DAYS) {
+      if (isEmployeePresent()) {
+        totalHours += FULL_TIME_HOUR;
+        totalWage += WAGE_PER_HOUR * FULL_TIME_HOUR;
+      }
+      days++;
     }
 
- 
-    static void displayWelcomeMessage() {
-        System.out.println("Welcome to Employee Wage Computation Program");
-    }
+    return totalWage;
+  }
 
-
-    static int getEmployeeType() {
-        Random random = new Random();
-        return random.nextInt(3);
-    }
-
-
-    static int calculateDailyWage(int employeeType) {
-
-        int workHours = 0;
-
-        switch (employeeType) {
-            case 1: 
-                workHours = FULL_DAY_HOURS;
-                break;
-
-            case 2: 
-                workHours = PART_TIME_HOURS;
-                break;
-
-            default: 
-                workHours = 0;
-        }
-
-        return workHours * WAGE_PER_HOUR;
-    }
-
-
-    static void displayWage(int employeeType, int wage) {
-
-        switch (employeeType) {
-            case 1:
-                System.out.println("Employee is Full Time");
-                break;
-
-            case 2:
-                System.out.println("Employee is Part Time");
-                break;
-
-            default:
-                System.out.println("Employee is Absent");
-        }
-
-        System.out.println("Daily Wage = " + wage);
-    }
 }
-  
-  
-
